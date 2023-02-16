c6c32ac761c5417ef910468952e22985d4beff06
robertobustillos <luisr.bustillos@potros.itson.edu.mx>
2023-02-15 22:07:56 -0700

    primer commit práctica 6

---
+++ app/src/main/java/bustillos/roberto/popcornfactory/MainActivity.kt
@@ -0,0 +1,11 @@
+ackage bustillos.roberto.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

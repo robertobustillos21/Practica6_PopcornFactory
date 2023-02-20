717756cf2c65f232a4a1d97b408ee35ec9b71c38
robertobustillos <luisr.bustillos@potros.itson.edu.mx>
2023-02-20 16:05:13 -0700

    primera parte p6 final

--- app/src/main/java/bustillos/roberto/popcornfactory/MovieDetail.kt
+++ app/src/main/java/bustillos/roberto/popcornfactory/MovieDetail.kt
@@ -2,10 +2,26 @@ package bustillos.roberto.popcornfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        var bundle = intent.extras
+
+        if(bundle != null){
+
+            var detalle_sinopsis = findViewById<TextView>(R.id.detalle_sinopsis)
+            var detalle_imagen = findViewById<ImageView>(R.id.detalle_imagen)
+            var detalle_titulo = findViewById<TextView>(R.id.detalle_titulo)
+
+            detalle_sinopsis.text = bundle.getString("sinopsis")
+            detalle_imagen.setImageResource(bundle.getInt("image"))
+            detalle_titulo.text = title
+
+        }
     }
 }
